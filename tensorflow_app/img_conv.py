4f878bc4b9b761c7260186055a61d15e14b5423f
leesia <leesia@aliyun.com>
2018-11-19 20:34:52 +0800

    更新ai

---
+++ tensorflow_app/img_conv.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import numpy as np
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import matplotlib.pyplot as plt  # plt 用于显示图片
+import matplotlib.image as mpimg  # mpimg 用于读取图片
+
+myimg = mpimg.imread('mm.jpg')  # 读取和代码处于同一目录下的图片
+plt.imshow(myimg)  # 显示图片
+plt.axis('off')  # 不显示坐标轴
+plt.show()
+print(myimg.shape)
+
+full = np.reshape(myimg, [1, myimg.shape[0], myimg.shape[1], myimg.shape[2]])
+inputfull = tf.Variable(tf.constant(1.0, shape=[1, myimg.shape[0], myimg.shape[1], myimg.shape[2]]))
+
+filter = tf.Variable(tf.constant([[-1.0, -1.0, -1.0], [0, 0, 0], [1.0, 1.0, 1.0],
+                                  [-2.0, -2.0, -2.0], [0, 0, 0], [2.0, 2.0, 2.0],
+                                  [-1.0, -1.0, -1.0], [0, 0, 0], [1.0, 1.0, 1.0]], shape=[3, 3, 3, 1]))
+
+op = tf.nn.conv2d(inputfull, filter, strides=[1, 1, 1, 1], padding='SAME')  # 3个通道输入，生成1个feature ma
+o = tf.cast(((op - tf.reduce_min(op)) / (tf.reduce_max(op) - tf.reduce_min(op))) * 255, tf.uint8)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    t, f = sess.run([o, filter], feed_dict={inputfull: full})
+    # print(f)
+    t = np.reshape(t, [myimg.shape[0], myimg.shape[1]])
+
+    plt.imshow(t, cmap='Greys_r')  # 显示图片
+    plt.axis('off')  # 不显示坐标轴
+    plt.show()
